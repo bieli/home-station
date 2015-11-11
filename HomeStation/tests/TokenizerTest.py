0206b55cdd3ebf80cf291eda54fb814467f5d1c2
Marcin Bielak <marcin.bieli@gmail.com>
2015-11-11 22:35:59 +0000

    update unit tests for Tokenizer

--- HomeStation/tests/TokenizerTest.py
+++ HomeStation/tests/TokenizerTest.py
@@ -1,23 +1,35 @@
 import unittest
 
+from HomeStation.message import DataMessage_pb2
 from HomeStation.tools.tokenizer import Tokenizer
 
 
 class TokenizerTest(unittest.TestCase):
+    def setUp(self):
+        self.unit = Tokenizer()
 
-  def test_should_prepare_token_with_no_empty_args(self):
-      # given
-      expected_token = "58d5a909d0dcd14171190854e9b72a7e55c490d9977ed4c73bc3ac8fdeb7530b"
-      stationId = "example--station--id"
-      apiKey = "example--api--key"
-      timestamp = 1447280723
-      tok = Tokenizer()
+    def test_should_prepare_token_with_no_empty_args(self):
+        # given
+        expected_token = "58d5a909d0dcd14171190854e9b72a7e55c490d9977ed4c73bc3ac8fdeb7530b"
+        stationId = "example--station--id"
+        apiKey = "example--api--key"
+        timestamp = 1447280723
 
-      # when
-      token = tok.prepare_token(stationId, apiKey, timestamp)
+        # when
+        token = self.unit.prepare_token(stationId, apiKey, timestamp)
 
-      # then
-      self.assertEqual(expected_token, token)
+        # then
+        self.assertEqual(expected_token, token)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_should_validate_token_for_good_token_in_data_message(self):
+        # given
+        validated_token = "58d5a909d0dcd14171190854e9b72a7e55c490d9977ed4c73bc3ac8fdeb7530b"
+
+        data_message = DataMessage_pb2.DataMessage()
+        data_message.token = validated_token
+
+        # when
+        result = self.unit.validate_token(data_message, validated_token)
+
+        # then
+        self.assertTrue(result)
